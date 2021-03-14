2099d0166e94a27202a2faa8f8c93d7a8983a45a
crazydonkey47 <61045967+crazydonkey47@users.noreply.github.com>
2021-03-14 12:54:23 +0100

    Create main.java

---
+++ zad1/main.java
@@ -0,0 +1,12 @@
+package com.company.z1;
+
+public class main {
+    public static void main(String[] args) {
+        Dog pies = new Dog("Maks", "Brazowy");
+        pies.makeSound();
+
+        Cat kot = new Cat("Miauczek", "Bialego");
+        kot.makeSound();
+
+    }
+}
