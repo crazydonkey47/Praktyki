a203f3088af7b4880923aecc33b9ebc0adc00c99
crazydonkey47 <61045967+crazydonkey47@users.noreply.github.com>
2021-03-14 12:52:09 +0100

    Create Animal.java

---
+++ zad1/Animal.java
@@ -0,0 +1,11 @@
+package com.company.zad1;
+
+public class Animal {
+    protected String name;
+    protected String color;
+
+    public Animal(String name, String color){
+        this.name = name;
+        this.color = color;
+    }
+}
