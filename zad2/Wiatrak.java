d9136b7766aa3d3ca15bb74e75bd413f331beef0
crazydonkey47 <61045967+crazydonkey47@users.noreply.github.com>
2021-03-14 12:31:24 +0100

    Create Wiatrak.java

---
+++ zad2/Wiatrak.java
@@ -0,0 +1,25 @@
+package com.company.z2;
+
+public class Wiatrak {
+    int speed;
+    boolean on;
+    double radius;
+    String color;
+
+    public Wiatrak(int speed, boolean on, double radius, String color){
+        this.speed = speed;
+        this.on = on;
+        this.radius = radius;
+        this.color = color;
+    }
+    Wiatrak(){
+        this.speed = Wiatrak_Level.SLOW.level;
+        this.on = false;
+        this.radius = 20;
+        this.color = "Black";
+    }
+    public void info(){
+        System.out.println("Predkosc wiatraka: " + this.speed + "\nWlaczony?: "+this.on+"\nPromien wiatraka: "+this.radius+"\nKolor: "+this.color);
+    }
+
+}
