7dd859fc6a03b0630aa628105f83225990976115
crazydonkey47 <61045967+crazydonkey47@users.noreply.github.com>
2021-03-14 12:32:09 +0100

    Create Wiatrak_Level.java

---
+++ zad2/Wiatrak_Level.java
@@ -0,0 +1,13 @@
+package com.company.zad2;
+
+public enum Wiatrak_Level {
+
+    SLOW(1),
+    MEDIUM(2),
+    FAST(3);
+
+    public int  level;
+    Wiatrak_Level(int level) {
+        this.level = level;
+    }
+}
