958f59234f144e91a2db4f0646993445a2f42230
crazydonkey47 <61045967+crazydonkey47@users.noreply.github.com>
2021-03-14 12:22:25 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class main {
+    public static void main(String[] args) {
+
+        square kwd = new square(2);
+        kwd.count_field();
+        kwd.count_circuit();
+    }
+}
